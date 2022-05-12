ef222a7543be95369311bbfb6f06302db436cba0
Pavel Zhuravlev <zhuravlev.pe@gmail.com>
2022-05-12 20:51:10 +0700

    solution

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhuravlev-pe/golang-united-school-homework-4.1
+
+go 1.17
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,20 @@
 package reverse_string
 
+func reverseSlice(runes []rune) {
+	if len(runes) < 2 {
+		return
+	}
+	to := len(runes) / 2
+
+	for i := 0; i < to; i++ {
+		with := len(runes) - 1 - i
+		runes[i], runes[with] = runes[with], runes[i]
+	}
+}
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	reverseSlice(runes)
+	output = string(runes)
 	return output
 }
